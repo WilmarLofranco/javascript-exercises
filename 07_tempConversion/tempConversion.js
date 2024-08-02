287b509199eb2cb5c16e6c61eb19479a94cb4863
Wilmar <wilmarlofranco@gmail.com>
2024-08-02 09:04:39 +0800

    Passed test 6 to 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celsius = (f - 32) * (5/9);
+  let rounded = Math.round(celsius *10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahrenheit = (c * 9/5) + 32
+  let rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
